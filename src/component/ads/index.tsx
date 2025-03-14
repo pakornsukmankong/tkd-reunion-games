edf2d065fe4d3a6727565a304f7b27fde2511c34
Pakorn Sukmankong <floksong.001@gmail.com>
2025-03-15 01:41:34 +0700

    feat: add picture

---
+++ src/component/ads/index.tsx
@@ -0,0 +1,71 @@
+const Ads = () => {
+  return (
+    <>
+      <div
+        style={{
+          border: '1px solid white',
+          borderColor: 'red',
+          width: '100%',
+          height: '100px',
+          backgroundColor: 'red',
+          marginBottom: '120px',
+        }}
+      >
+        <a
+          href="https://www.sbobetonline24.com/%E0%B8%97%E0%B8%B2%E0%B8%87%E0%B9%80%E0%B8%82%E0%B9%89%E0%B8%B2-sbobet/"
+          target="_blank"
+        >
+          <img
+            src="https://www.adorama.com/alc/wp-content/uploads/2021/05/bird-wings-flying-feature.gif"
+            width="100%"
+            height="200px"
+          />
+        </a>
+      </div>
+      <div
+        style={{
+          border: '1px solid white',
+          borderColor: 'red',
+          width: '100%',
+          height: '100px',
+          backgroundColor: 'red',
+          marginBottom: '120px',
+        }}
+      >
+        <a href="https://dookeela.live/" target="_blank">
+          <img
+            src="https://no-cdn.shortpixel.ai/client/to_avif,q_lossy,ret_wait/https://shortpixel.com/blog/wp-content/uploads/2023/12/nyan-cat.gif"
+            width="100%"
+            height="200px"
+          />
+        </a>
+      </div>
+      <div
+        style={{
+          border: '1px solid white',
+          borderColor: 'red',
+          width: '100%',
+          height: '100px',
+          backgroundColor: 'red',
+          marginBottom: '10px',
+        }}
+      >
+        <image>โฆษณา</image>
+      </div>
+      <div
+        style={{
+          border: '1px solid white',
+          borderColor: 'red',
+          width: '100%',
+          height: '100px',
+          backgroundColor: 'red',
+          marginBottom: '10px',
+        }}
+      >
+        <image>โฆษณา</image>
+      </div>
+    </>
+  )
+}
+
+export default Ads
--- src/component/home/index.tsx
+++ src/component/home/index.tsx
@@ -1,70 +1,7 @@
+import Ads from "../ads"
+
 const Home = () => {
   const isAds = false
-  const renderAds = () => {
-    if (isAds) {
-      return (
-        <>
-          <div
-            style={{
-              border: '1px solid white',
-              borderColor: 'red',
-              width: '100%',
-              height: '100px',
-              backgroundColor: 'red',
-              marginBottom: '200px',
-            }}
-          >
-            <a
-              href="https://www.sbobetonline24.com/%E0%B8%97%E0%B8%B2%E0%B8%87%E0%B9%80%E0%B8%82%E0%B9%89%E0%B8%B2-sbobet/"
-              target="_blank"
-            >
-              <img
-                src="https://www.adorama.com/alc/wp-content/uploads/2021/05/bird-wings-flying-feature.gif"
-                width="100%"
-                height="200px"
-              />
-            </a>
-          </div>
-          <div
-            style={{
-              border: '1px solid white',
-              borderColor: 'red',
-              width: '100%',
-              height: '100px',
-              backgroundColor: 'red',
-              marginBottom: '30px',
-            }}
-          >
-            <image>โฆษณา</image>
-          </div>
-          <div
-            style={{
-              border: '1px solid white',
-              borderColor: 'red',
-              width: '100%',
-              height: '100px',
-              backgroundColor: 'red',
-              marginBottom: '10px',
-            }}
-          >
-            <image>โฆษณา</image>
-          </div>
-          <div
-            style={{
-              border: '1px solid white',
-              borderColor: 'red',
-              width: '100%',
-              height: '100px',
-              backgroundColor: 'red',
-              marginBottom: '10px',
-            }}
-          >
-            <image>โฆษณา</image>
-          </div>
-        </>
-      )
-    }
-  }
 
   return (
     <>
@@ -84,7 +21,7 @@ const Home = () => {
         <div>Games</div>
         <div>Battle</div>
       </div>
-      {renderAds()}
+      {isAds ? <Ads/> : null}
     </>
   )
 }
