936b4506fc6ba3965eb094e5159173852b129bfd
Pakorn Sukmankong <floksong.001@gmail.com>
2025-03-15 02:22:30 +0700

    feat: add chakra ui

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,9 @@ const nextConfig: NextConfig = {
       },
     ]
   },
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
 };
 
 export default nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import { ChakraProvider, createSystem, defineConfig } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
+
+const config = defineConfig({
+  theme: {
+    tokens: {
+      colors: {},
+    },
+  },
+})
+
+const system = createSystem(config)
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <ChakraProvider value={system}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   )
 }
